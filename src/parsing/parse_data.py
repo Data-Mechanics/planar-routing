0814bfd5db8211f203b4731b4ea33b8585059b8a
Ben <bengetch@gmail.com>
2018-05-18 17:50:31 -0400

    parsing script and parsed data

---
+++ src/parsing/parse_data.py
@@ -0,0 +1,191 @@
+import json
+import sys
+
+
+def find_point(coords, point_list):
+    """
+    Coordinates represent either the source or destination [x,y] coordinates of a line.
+    Given a list of unique points, map one to the given coordinates.
+    """
+
+    for p in point_list:
+        if p["coordinates"] == coords:
+            return p["idx"]
+
+    print("Couldn't find the point -- {}!\n".format(str(point_list)))
+
+
+def filter_coords(coords):
+    """
+    Remove duplicate coordinates in LineString data.
+    """
+
+    unique = []
+    [unique.append(item) for item in coords if item not in unique]
+
+    return unique
+
+
+def assign_point_ids(point_list):
+    """
+    For each point, assign a unique id.
+    """
+
+    i = 0
+    ret = []
+
+    for point in point_list:
+        coords = point["coordinates"]
+        next_list = point["properties"]["next"]
+        entry = build_point_entry(coords, next_list, i)
+        ret.append(entry)
+        i += 1
+
+    return ret
+
+
+def build_line_entry(coords, start, end):
+    """
+    Construct a new LineString with additional data.
+    """
+
+    entry = \
+        {
+            "geometry":
+                {
+                    "coordinates": filter_coords(coords),
+                    "type": "LineString"
+                },
+            "properties":
+                {
+                    "start": start,
+                    "end": end
+                },
+            "type": "Feature"
+        }
+
+    return entry
+
+
+def build_point_entry(coords, next_list, idx, adjacent=None):
+    """
+    Add an ID to a Point.
+    """
+
+    entry = \
+        {
+            "coordinates": coords,
+            "properties":
+                {
+                    "next": next_list,
+                    "adjacent": adjacent
+                },
+            "type": "Point",
+            "idx": idx
+        }
+
+    return entry
+
+
+def build_line_records(line_list, point_list):
+    """
+    Determine start and end points for each line.
+    """
+
+    ret = []
+
+    for line in line_list:
+
+        coords = line["geometry"]["coordinates"]
+
+        start_coords = coords[0]
+        end_coords = coords[-1]
+
+        start_id = find_point(start_coords, point_list)
+        end_id = find_point(end_coords, point_list)
+
+        entry = build_line_entry(coords, start_id, end_id)
+        ret.append(entry)
+
+    return ret
+
+
+def _build_adjacency_lists(line_list, point):
+
+    adjacent = []
+
+    idx = point["idx"]
+
+    for line in line_list:
+
+        if line["properties"]["start"] == idx:
+            adjacent.append(line["properties"]["end"])
+        elif line["properties"]["end"] == idx:
+            adjacent.append(line["properties"]["start"])
+        else:
+            continue
+
+    ret = build_point_entry(point["coordinates"], point["properties"]["next"], point["idx"], adjacent)
+
+    return ret
+
+
+def build_adjacency_lists(line_list, point_list):
+
+    ret = []
+
+    for point in point_list:
+
+        entry = _build_adjacency_lists(line_list, point)
+        ret.append(entry)
+
+    return ret
+
+
+def build_full_data(datapoints):
+
+    ret = \
+        {
+            "features": datapoints,
+            "type": "geoql"
+        }
+
+    return json.dumps(ret, indent=4, separators=(',', ': '))
+
+
+if __name__ == '__main__':
+
+    lines = []
+    points = []
+
+    with open(sys.argv[1], 'r', encoding='utf8') as in_file:
+
+        json_data = json.load(in_file)
+
+        for f in json_data["features"]:
+
+            if "geometry" in f:
+                lines.append(f)
+            else:
+                points.append(f)
+
+    points_with_ids = assign_point_ids(points)
+    line_records = build_line_records(lines, points_with_ids)
+    points_with_adjacency = build_adjacency_lists(line_records, points_with_ids)
+
+    full_data = line_records + points_with_adjacency
+
+    with open(sys.argv[2], 'w', encoding='utf8') as out_file:
+
+        out_data = build_full_data(full_data)
+
+        out_file.write(out_data)
+
+
+
+
+
+
+
+
+
--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,6 @@
 
-module.exports =
-	{
-	randomColor: function()
+
+	function randomColor()
 	{
 		var color = '', letters = '0123456';
 		for (var i=0; i<6; i++)
@@ -9,9 +8,10 @@ module.exports =
 			color += letters[Math.floor(Math.random() * letters.length)];
 		}
 		return '#' + color;
-	},
+	}
 
-	onEachFeature: function(feature, layer)
+	/*
+	function onEachFeature(feature, layer)
 	{
 		if (feature.properties)
 		{
@@ -24,7 +24,78 @@ module.exports =
 			layer.bindPopup(popupContent);
 		}
 	}
-	};
+	*/
+
+	function pointLayer(feature, latlng)
+	{
+
+	var node = L.circleMarker
+	(
+		latlng,
+		{radius:4, weight:0.1, fillColor:"#666666", color:"#666666", opacity:1, fillOpacity:1}
+	);
+
+	var colors = ['#FF0000', '#00FF00', '#0000FF', '#FF00FF', '#FFF000'];
+
+	nodes.push({'dat':feature, 'vis':node, 'idx': node_idx});
+	node.nodeid = node_idx;
+
+	node_idx++;
+
+	node.on('click', function()
+	{
+
+		nodes[node.nodeid].vis.setStyle({fillColor: "#FFFFFF", color: "#FFFFFF"});
+
+		if (clicks == 0)
+		{
+			route.start = node.nodeid;
+		}
+		else if (clicks == 1)
+		{
+			route.end = node.nodeid;
+			// *** routing function here ***
+		}
+		else
+		{
+			clicks = 0;
+			route.start = node.nodeid;
+			route.end = 999;
+		}
+
+		clicks++;
+
+		console.log(node.nodeid);
+
+		/*
+		console.log(feature);
+		console.log(node);
+		console.log(nodes);
+	  */
+
+		for (var i = 0; i < feature.properties.next.length; i++)
+		{
+			for (var j = 0; j < feature.properties.next[i].length; j++)
+			{
+				nodes[feature.properties.next[i][j]].vis.setStyle({fillColor:colors[i], color:colors[i]});
+				//console.log(nodes[feature.properties.next[i][j]].dat.coordinates);
+			}
+		}
+
+		/*
+		console.log(route.start);
+		console.log(route.end);
+		*/
+
+	});
+
+
+
+	return node;
+
+	}
+
+
 
 
 

799b90ec9f0952646011869ba285f018c0797a5b
Ben <bengetch@gmail.com>
2018-05-25 13:01:10 -0400

    handle case where destination node is unreachable from start

--- src/routing.js
+++ src/routing.js
@@ -9,78 +9,6 @@ var route =
 	"path": []
 };
 
-Array.prototype.contains = function(needle)
-{
-	/*
-	Determine if an element is in an array. Array
-	is assumed to be sorted in ascending order.
-	 */
-
-	var min = 0;
-	var max = this.length - 1;
-	var guess;
-
-	while (min <= max) {
-		guess = Math.floor((min + max) / 2);
-
-		if (this[guess] === needle) {
-			return true;
-		}
-		else {
-			if (this[guess] < needle) {
-				min = guess + 1;
-			}
-			else {
-				max = guess - 1;
-			}
-		}
-	}
-
-	return false;
-};
-
-Array.prototype.containsUnsorted = function(needle)
-{
-
-	for (var i = 0; i < this.length; i++)
-	{
-		if (this[i] === needle)
-		{
-			return true;
-		}
-	}
-
-	return false;
-};
-
-Array.prototype.containsPoly = function(needle)
-{
-	/*
-	Determine if an element is within a polygon represented
-	by the points of an array. Points are assumed to be in
-	either clockwise or counterclockwise order.
-	 */
-
-	var numVert = this.length;
-	var x = needle[0];
-	var y = needle[1];
-	var inside = false;
-
-	for (var i = 0, j = numVert - 1; i < numVert; j = i++)
-	{
-		var xi = this[i][0], yi = this[i][1];
-		var xj = this[j][0], yj = this[j][1];
-
-		var intersect = ( (yi > y) !== (yj > y) ) && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
-		if (intersect)
-		{
-			inside = !inside;
-		}
-	}
-
-	return inside;
-};
-
 
 function updateRoute(current_list, route_dict)
 {
@@ -133,13 +61,6 @@ function router(route_dict, nodes)
 	Follow adjacent nodes from routing table until destination is reached.
 	 */
 
-	/*
-	TODO - still producing cycles in some cases. Because polygons overlap, the routing
-	function will get stuck jumping between two polygons in certain cases. Need to fix
-	by checking first whether the adjacency set for a given next_list is already contained
-	in the current path before using it.
-	 */
-
 	var current_node = route_dict.start;
 
 	while (current_node !== route_dict.end)
@@ -155,12 +76,12 @@ function router(route_dict, nodes)
 
 			if (nodes[current_node].dat.properties.next[i][0].containsUnsorted(route_dict.end))
 			{
-				/*
-				TODO - check for cycles here
-				 */
 				current_list = nodes[current_node].dat.properties.next[i];
 				next_node = updateRoute(current_list, route_dict);
 
+				/*
+				Check if path already includes this node (eliminates cycles).
+				 */
 				if (!route_dict.path.containsUnsorted(next_node))
 				{
 					route_dict.path.push(next_node);
@@ -169,13 +90,10 @@ function router(route_dict, nodes)
 				}
 			}
 			/*
-			Then test if the destination nodes is inside the polygon represented by the vertices.
+			Then check if the destination nodes is inside the polygon represented by the vertices.
 			 */
 			else
 			{
-				/*
-				TODO - check for cycles here
-				 */
 				var nextLength = nodes[current_node].dat.properties.next[i][0].length;
 
 				if (nextLength > 2)
@@ -193,6 +111,9 @@ function router(route_dict, nodes)
 						current_list = nodes[current_node].dat.properties.next[i];
 						next_node = updateRoute(current_list, route_dict);
 
+						/*
+						Check if path already includes this node (eliminates cycles).
+						 */
 						if (!route_dict.path.containsUnsorted(next_node))
 						{
 							route_dict.path.push(next_node);
@@ -202,6 +123,11 @@ function router(route_dict, nodes)
 					}
 				}
 			}
+		}
+		if (i === nodes[current_node].dat.properties.next.length && route_dict.path.containsUnsorted(current_node))
+		{
+			alert("Destination is unreachable from that point.");
+			break;
 		}
 	}
 
